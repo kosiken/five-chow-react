bf890a154c7df483144e3087422259bd3b0742ad
kosiken <allisonkosy@gmail.com>
2020-06-06 08:53:09 +0100

    remove unnecessar imports

--- src/components/ResturantList.js
+++ src/components/ResturantList.js
@@ -1,7 +1,7 @@
 import React,{useState, useEffect} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+
 import Grid from "@material-ui/core/Grid"
-import Fab from "@material-ui/core/Fab"
+
 
 import Resturant from "./Resturant"
 import hamburger from "../assets/hamburger.jpg"
@@ -15,25 +15,10 @@ const cards =  [
     { title: 'Resturant three', src: meat, flex: 6 },
   
   ]
-const restutants = [];
-const useStyles = makeStyles((theme)=> ({
-    gridl: {
-        margin: "0 auto"
-    },
-      fab: {
-    position: 'absolute',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}) )
-for(let i = 0; i< 15; i++){
-    restutants.push({
-        name:'Resturant ' + i,
-        picture: cards[i%3].src
-    })
-}
+
+
 function ResturantList() {
-    const classes = useStyles();
+
     return (
         <div>
             <Grid style={{
